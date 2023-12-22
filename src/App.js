be6c4e7660ab0ba5d3b95fa64a4c596616d77df2
Sophia S. <sophia0791@icloud.com>
2023-12-22 11:33:45 +0100

    add function with article

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWolrdArticle() {
+  return (
+    <article>
+      <h1>Christmas Coding</h1>
+      <p>{"There's nothing more geilo meilo than Christmas Coding!"}</p>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWolrdArticle />;
 }
